b92e113bbf838ea73029efa1847a8bba57eb12b6
Alexander Angelov <rapgeneral2@gmail.com>
2018-11-12 14:00:12 +0200

    Added identity

---
+++ BedeSlots/BedeSlots/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BedeSlots.Areas.Identity.Data;
+using BedeSlots.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BedeSlots.Areas.Identity.IdentityHostingStartup))]
+namespace BedeSlots.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BedeSlotsContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BedeSlotsContextConnection")));
+
+                services.AddDefaultIdentity<BedeSlotsUser>()
+                    .AddEntityFrameworkStores<BedeSlotsContext>();
+            });
+        }
+    }
+}
