9d96a2c4c9108ffdf42229aafd9b12a9a0b9330d
Stanchev <astanchev100@gmail.com>
2018-11-23 13:04:28 +0200

    Revert "added bank details service"
    This reverts commit 94e35c4df31b4811e54a9a37eed82cc8205cc85c.

--- BedeSlots/BedeSlots.Services/Contracts/ITransactionService.cs
+++ BedeSlots/BedeSlots.Services/Contracts/ITransactionService.cs
@@ -1,15 +1,11 @@
-﻿using BedeSlots.ViewModels;
-using BedeSlots.ViewModels.Enums;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BedeSlots.Services.Contracts
 {
     public interface ITransactionService
     {
-        Task<TransactionViewModel> CreateTransactionAsync(TypeOfTransaction type, string description, decimal amount, string userId);
-        Task<ICollection<TransactionViewModel>> SearchTransactionAsync(string username, int min, int max, ICollection<string> types);
+
     }
 }
