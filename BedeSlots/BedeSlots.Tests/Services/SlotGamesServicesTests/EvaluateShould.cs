7c1fa1e8d8b2e865081ab6d31f1c9eaa5de25306
RapGeneral <rapgeneral3@gmail.com>
2018-11-28 22:08:53 +0200

    SlotGames services done and unit tested
    Fixed unit test
    
    The algorithm now works when the first element is the wild card.

---
+++ BedeSlots/BedeSlots.Tests/Services/SlotGamesServicesTests/EvaluateShould.cs
@@ -0,0 +1,77 @@
+﻿using BedeSlots.Services;
+using BedeSlots.ViewModels.Enums;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace BedeSlots.Tests.Services.SlotGamesServicesTests
+{
+    [TestClass]
+    public class EvaluateShould
+    {
+        [DataTestMethod]
+        [DataRow(2, 3)]
+        [DataRow(3, 2)]
+        [DataRow(2, 2)]
+        public void ThrowArgumentExpcetion_WhenTheMatrixIsSamllerThan3x3(int n, int m)
+        {
+            //Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var sut = new SlotGamesServices(memoryCache);
+            var matrix = new List<List<GameItemChanceOutOf100>>();
+            for (int i = 0; i < n; i++)
+            {
+                var row = new List<GameItemChanceOutOf100>();
+                for (int j = 0; j < m; j++)
+                {
+                    row.Add(GameItemChanceOutOf100.Apple);
+                }
+                matrix.Add(row);
+            }
+            //Act && Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Evaluate(matrix));
+        }
+        //Basically data rows with arrays.
+        private static IEnumerable<object[]> FirstSlotMatrix =>
+            new List<object[]> {
+                new object[] {new List<List<GameItemChanceOutOf100>> {
+                    new List<GameItemChanceOutOf100>{ GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple},
+                    new List<GameItemChanceOutOf100>{ GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.Pen},
+                    new List<GameItemChanceOutOf100>{GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.PPAP } },
+                    (3*(int)Enum.Parse(typeof(GameItemCoeffsOutOf10), GameItemChanceOutOf100.Apple.ToString()))/10.0//Coeff actual value
+                },
+                new object[] {new List<List<GameItemChanceOutOf100>> {
+                    new List<GameItemChanceOutOf100>{ GameItemChanceOutOf100.PPAP, GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple},
+                    new List<GameItemChanceOutOf100>{ GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.Pen},
+                    new List<GameItemChanceOutOf100>{GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.PPAP } },
+                    (2*(int)Enum.Parse(typeof(GameItemCoeffsOutOf10), GameItemChanceOutOf100.Apple.ToString()))/10.0//Coeff actual value
+                },
+                new object[] {new List<List<GameItemChanceOutOf100>>  {
+                    new List<GameItemChanceOutOf100> { GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple},
+                    new List<GameItemChanceOutOf100> { GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.PPAP},
+                    new List<GameItemChanceOutOf100> {GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.PPAP } },
+                    (5*(int)Enum.Parse(typeof(GameItemCoeffsOutOf10), GameItemChanceOutOf100.Apple.ToString())+
+                    (int)Enum.Parse(typeof(GameItemCoeffsOutOf10), GameItemChanceOutOf100.PPAP.ToString()))/10.0//coeff actual value
+                },
+                new object[] {new List<List<GameItemChanceOutOf100>>  {
+                    new List<GameItemChanceOutOf100> { GameItemChanceOutOf100.PPAP, GameItemChanceOutOf100.PPAP, GameItemChanceOutOf100.PPAP},
+                    new List<GameItemChanceOutOf100> { GameItemChanceOutOf100.PPAP, GameItemChanceOutOf100.PPAP, GameItemChanceOutOf100.PPAP},
+                    new List<GameItemChanceOutOf100> {GameItemChanceOutOf100.Apple, GameItemChanceOutOf100.Pineapple, GameItemChanceOutOf100.PPAP } },
+                    (6*(int)Enum.Parse(typeof(GameItemCoeffsOutOf10), GameItemChanceOutOf100.PPAP.ToString()))/10.0//coeff actual value
+                }
+            };
+        [TestMethod]
+        [DynamicData(nameof(FirstSlotMatrix))]
+        public void CorrectlyCalculateCoefficients(List<List<GameItemChanceOutOf100>>slotMatrix, double actualCoeff)
+        {
+            //Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var sut = new SlotGamesServices(memoryCache);
+            //Act
+            var coeff = sut.Evaluate(slotMatrix);
+            //Assert
+            Assert.IsTrue(coeff == actualCoeff);
+        }
+    }
+}
---
+++ BedeSlots/BedeSlots.Tests/Services/SlotGamesServicesTests/RunShould.cs
@@ -0,0 +1,64 @@
+﻿using BedeSlots.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BedeSlots.Tests.Services.SlotGamesServicesTests
+{
+    [TestClass]
+    public class RunShould
+    {
+        [DataTestMethod]
+        [DataRow(2, 3)]
+        [DataRow(3, 2)]
+        [DataRow(2, 2)]
+        public void ThrowArgumentExpcetion_WhenTheMatrixIsSamllerThan3x3(int n, int m)
+        {
+            //Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var sut = new SlotGamesServices(memoryCache);
+            //Act && Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Run(n, m));
+        }
+
+        [DataTestMethod]
+        [DataRow(3, 3)]
+        [DataRow(3, 5)]
+        [DataRow(5, 7)]
+        public void CreateNxMFullMatrix(int n, int m)
+        {
+            //Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var sut = new SlotGamesServices(memoryCache);
+            bool hasCorrectDimentions = true;
+            bool doesntHave0Items = true; 
+            //0 Items will check two things:
+            //First, if every cell is populated
+            //Second, if there are any items, whose values are 0 (for whatever reason...)
+            //Act
+            var result = sut.Run(n, m);
+            //Assert
+            if(result.Count != n)
+            {
+                hasCorrectDimentions = false;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if(result[i].Count != m)
+                {
+                    hasCorrectDimentions = false;
+                    break;
+                }
+                for (int j = 0; j < m; j++)
+                {
+                    if((int)result[i][j] == 0)
+                    {
+                        doesntHave0Items = false;
+                    }
+                }
+            }
+            Assert.IsTrue(hasCorrectDimentions);
+            Assert.IsTrue(doesntHave0Items);
+        }
+    }
+}
